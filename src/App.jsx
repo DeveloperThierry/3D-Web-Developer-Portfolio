d1156cdfd9e6a05150b76356d68c4068b65b71ed
developerthierry <developerthierry@gmail.com>
2025-08-22 09:46:06 +0000

    Work Experience Timeline

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LogoSection from "./sections/LogoSection";
 import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
+import Experience from "./sections/Experience";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <ShowcaseSection />
       <LogoSection/>
       <FeatureCards/>
+      <Experience/>
     </>
   );
 }
