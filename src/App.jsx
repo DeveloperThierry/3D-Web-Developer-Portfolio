70bca550c9da6fb2679b09babc7fbb56abab0471
developerthierry <developerthierry@gmail.com>
2025-08-23 02:20:50 +0000

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Experience from "./sections/Experience";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 function App() {
   return (
     <>
@@ -19,6 +20,7 @@ function App() {
       <TechStack/>
       <Testimonials/>
       <Contact/>
+      <Footer/>
     </>
   );
 }
